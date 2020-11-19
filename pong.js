c5a8944c2908a26f87ca1024839c68b5a9f1702c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-19 01:47:21 +0100

    Arreglada hitbox y movimiento en paralelo

--- pong.js
+++ pong.js
@@ -35,8 +35,9 @@ class Bola{
         };
         var derecha=document.getElementById("derecha");
         var izquierda=document.getElementById("izquierda");
-        if(bola.posX >=derecha.x.animVal.value-bola.radio && bola.posX <=derecha.x.animVal.value+derecha.width.animVal.value && bola.posY+bola.radio>=derecha.y.animVal.value && bola.posY+bola.radio<=(derecha.y.animVal.value+derecha.height.animVal.value)){
+        if(bola.posX >=derecha.x.animVal.value-bola.radio && bola.posX <=derecha.x.animVal.value+derecha.width.animVal.value && bola.posY+bola.radio>=derecha.y.animVal.value && bola.posY-bola.radio<=(derecha.y.animVal.value+derecha.height.animVal.value)){
             bola.incX *= -1;
+            bola.incX-=1;
             if(Math.round(Math.random())*3==3){
                 if(bola.incY>0){
                     bola.incY+=1;
@@ -44,18 +45,18 @@ class Bola{
                 }else{
                     bola.incY+=-1;
                     if(bola.incY<-10)bola.incY=-9;
-                }
+                 }
             }
         }
-        if(bola.posX-bola.radio<=izquierda.x.animVal.value+izquierda.width.animVal.value && bola.posX >=izquierda.x.animVal.value && bola.posY+bola.radio>=izquierda.y.animVal.value && bola.posY+bola.radio<=izquierda.y.animVal.value+izquierda.height.animVal.value){
-            bola.incX*=-1
+        if(bola.posX-bola.radio<=izquierda.x.animVal.value+izquierda.width.animVal.value && bola.posX >=izquierda.x.animVal.value && bola.posY+bola.radio>=izquierda.y.animVal.value && bola.posY-bola.radio<=izquierda.y.animVal.value+izquierda.height.animVal.value){
+            bola.incX*=-1;
+            bola.incX+=1;
             if(Math.round(Math.random())*3==3){
                 if(bola.incY>0){
                     bola.incY+=1;
                     if(bola.incY>10)bola.incY=9;
                 }else{
                     bola.incY+=-1;
-                    if(bola.incY<-10)bola.incY=-9;
                 }
             }
         }
@@ -76,10 +77,10 @@ class Bola{
             if(Math.round(Math.random())*3==3){
                 if(bola.incX>0){
                     bola.incX+=2;
-                    if(bola.incX>10)bola.incX=9;
+                    if(bola.incX>20)bola.incX=19;
                 }else{
                     bola.incX-=2;
-                    if(bola.incX<-10)bola.incX=-9;
+                    if(bola.incX<-20)bola.incX=-19;
                 }
             }
         }
@@ -89,10 +90,10 @@ class Bola{
             if(Math.round(Math.random())*3==3){
                 if(bola.incX>0){
                     bola.incX+=2;
-                    if(bola.incX>10)bola.incX=9;
+                    if(bola.incX>20)bola.incX=19;
                 }else{
                     bola.incX-=2;
-                    if(bola.incX<-10)bola.incX=-9;
+                    if(bola.incX<-20)bola.incX=-19;
                 }
             }
         }
@@ -113,19 +114,51 @@ class Palo{
         rect.setAttributeNS(null,"id",lado);
         rect.setAttributeNS(null,"fill","white");
         document.getElementsByTagName("svg")[0].appendChild(rect);
-
+        var w=false,s=false,up=false,down=false;
+        var letras=Array(w,s,up,down);
         document.addEventListener("keydown",event =>{
-            if(event.key=="w" || event.key=="W"){
+            if(event.key=="w" || event.key=="W"){letras[0]=true;}
+            if(event.key=="s" || event.key=="S"){letras[1]=true;}
+            if(event.key=="ArrowUp"){letras[2]=true;}
+            if(event.key=="ArrowDown"){letras[3]=true;}
+
+            if(letras[0] && letras[2]){
                 document.getElementById("izquierda").setAttribute("y",document.getElementById("izquierda").y.animVal.value-20);
+                document.getElementById("derecha").setAttribute("y",document.getElementById("derecha").y.animVal.value-20);
+            }else{
+                if(letras[1] && letras [3]){
+                    document.getElementById("izquierda").setAttribute("y",document.getElementById("izquierda").y.animVal.value+20);
+                    document.getElementById("derecha").setAttribute("y",document.getElementById("derecha").y.animVal.value+20);
+                }else{
+                    if(letras[0] && letras[1]){
+                        document.getElementById("izquierda").setAttribute("y",document.getElementById("izquierda").y.animVal.value-20);
+                        document.getElementById("izquierda").setAttribute("y",document.getElementById("izquierda").y.animVal.value+20);
+                    }else{
+                        if(letras[2] && letras[3]){
+                            document.getElementById("derecha").setAttribute("y",document.getElementById("derecha").y.animVal.value-20);
+                            document.getElementById("derecha").setAttribute("y",document.getElementById("derecha").y.animVal.value+20);
+                        }else{
+                            if(letras[0]){document.getElementById("izquierda").setAttribute("y",document.getElementById("izquierda").y.animVal.value-20);}
+                            if(letras[1]){document.getElementById("izquierda").setAttribute("y",document.getElementById("izquierda").y.animVal.value+20);}
+                            if(letras[2]){document.getElementById("derecha").setAttribute("y",document.getElementById("derecha").y.animVal.value-20);}
+                            if(letras[3]){document.getElementById("derecha").setAttribute("y",document.getElementById("derecha").y.animVal.value+20);}
+                        }
+                    }
+                }
+            }
+        });
+        document.addEventListener("keyup",event =>{
+            if(event.key=="w" || event.key=="W"){
+                letras[0]=false;
             }
             if(event.key=="s" || event.key=="S"){
-                document.getElementById("izquierda").setAttribute("y",document.getElementById("izquierda").y.animVal.value+20);
+                letras[1]=false;
             }
             if(event.key=="ArrowUp"){
-                document.getElementById("derecha").setAttribute("y",document.getElementById("derecha").y.animVal.value-20);
+                letras[2]=false;
             }
             if(event.key=="ArrowDown"){
-                document.getElementById("derecha").setAttribute("y",document.getElementById("derecha").y.animVal.value+20);
+                letras[3]=false;
             }
         });
     }
@@ -163,3 +196,4 @@ var m1=0;
 var m2=0;
 nuevaBola();
 setInterval(anima,30);
+document.addEventListener("mousemove",()=>{console.log(window.event.clientX+" | "+window.event.clientY)})
